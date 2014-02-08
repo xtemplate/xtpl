9eeb6d4dc358e5b0a4633a0c1316918ea41ddc85
wondger <wondger@gmail.com>
2014-02-08 16:54:20 +0800

    xtemplate for node.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * #
+ * @author yanmu.wj@taobao.com
+ * @date 2014-02-08
+ */
+
+/*!*/
+module.exports = exports = require("./lib/index");
---
+++ test/index.js
@@ -0,0 +1,20 @@
+/*!
+ * # 
+ * @author yanmu.wj@taobao.com
+ * @date 2014-02-08
+ */
+
+/*!*/
+var path = require("path");
+var app = require("express")();
+
+app.set("view engine", "xtpl");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", function(req, res) {
+    res.render("index", {title: "xtpl engine!"})
+});
+
+app.listen(3001, function() {
+    console.log("xtpl test sever start : 3001")
+});
