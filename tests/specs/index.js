9a81c8e9f0d52d414a234b5f399091ab701a069e
yiminghe <yiminghe@gmail.com>
2014-09-10 20:49:16 +0800

    support koa. Fixes #5

--- tests/specs/index.js
+++ tests/specs/index.js
@@ -1,6 +1,8 @@
 var xtpl = require('../../');
 var path = require('path');
+var xtplKoa = require('../../lib/koa');
 var expect = require('chai').expect;
+
 describe('xtpl', function () {
     it('can get XTemplate engine', function () {
         expect(xtpl.XTemplate).not.to.equal(undefined);
@@ -15,5 +17,26 @@ describe('xtpl', function () {
             expect(data).to.equal('<&gt;');
             done();
         });
+    });
+
+    it('works for koa', function (done) {
+        var app = xtplKoa(require('koa')(), {
+            views: path.resolve(__dirname, '../fixture/')
+        });
+        app.use(function *() {
+            var html = yield* this.render('main', {
+                y: '<',
+                x: '>'
+            });
+            expect(html).to.equal('<&gt;');
+        });
+        app.listen(9000);
+        var request = require('request');
+        setTimeout(function () {
+            request({url: 'http://localhost:9000'}, function (error, response, body) {
+                expect(body).to.equal('<&gt;');
+                done();
+            });
+        }, 100);
     });
 });
